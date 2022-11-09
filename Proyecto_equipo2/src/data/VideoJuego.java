c77a22311ad34a195133e3af241f1138eb7465e8
alvaroblancosangines <alvaroblancones@gmail.com>
2022-11-09 21:59:41 +0100

    Interface Videojuego y clase VideojuegoImpl listo

---
+++ Proyecto_equipo2/src/data/VideoJuego.java
@@ -0,0 +1,19 @@
+package data;
+
+import java.util.List;
+
+/**
+ * He creadp una interfaz con un método alta. De momento no aparece la clase
+ * juego
+ * 
+ * @author Álvaro Blanco Sanginés
+ * @Equipo equipo 2
+ *
+ */
+public interface VideoJuego {
+	/**
+	 * @param no contiene ningún parámetro
+	 */
+	List<Object> altaJuego();
+
+}
